9b7148559371a67d037aede2e3be3b610ec625a0
Willie Jones <williejones432@yahoo.com>
2023-10-12 11:48:16 -0400

    added hour elements to HTML

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,10 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// ensures that the code isn't run until the browser has finished rendering all the elements in the html.
 $(document).ready(function() {
-  //let saveButton = document.querySelector('.saveBtn')
-  //let tasks = document.querySelector('textarea')
+
+  // Displays the current date & Time in the header of the page
+  let currentTime = dayjs().format('MMM D, YYYY (hh:mm:ss)');
+  $('#currentDay').html(currentTime);
+
   // Event listener for save button and saves to local storage when clicked
   $('.saveBtn').on('click', () => {
     const hour = $(this).parent().attr('id');
@@ -25,7 +26,5 @@ $(document).ready(function() {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // Displays the current date & Time in the header of the page
-  let currentTime = dayjs().format('MMM D, YYYY (hh:mm:ss)');
-  $('#currentDay').html(currentTime);
+  
 });
