ac450689349d0125903b3b1d3c8e30f48cb3d39f
Willie Jones <williejones432@yahoo.com>
2023-10-17 20:02:39 -0500

    Updated JS functions and time function

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,13 @@
 // ensures that the code isn't run until the browser has finished rendering all the elements in the html.
 $(document).ready(function() {
-
+  
   // Displays the current date & Time in the header of the page
-  let currentTime = dayjs().format('MMM D, YYYY (hh:mm:ss)');
-  $('#currentDay').html(currentTime);
+  function updateTime() {
+    const currentDate = dayjs().format('MMM D, YYYY (hh:mm:ss)')
+    $('#currentDay').html(currentDate);
+  }
+  //Constantly updates time on page
+  setInterval(updateTime, 1000);
 
   //Background colors change based on hour in the day. Past hours, present hours, and future hours.
   function scheduleColorChange () {
@@ -18,21 +22,14 @@ $(document).ready(function() {
       }
     }
   }
-  
 //calls color change function
 scheduleColorChange()
 
   // Event listener for save button and saves to local storage when clicked
-  $('.saveBtn').on('click', () => {
+  //$('.saveBtn').on('click', function() {
+    //const taskData = $('#taskData').val();
+    //const hour = $('.hour').val();
 
-    const hour = $(this).data('hour');
-    const toDo = $(`input`).val();
-
-    localStorage.setItem('${hour}',toDo);
-
-    //alert('Task Saved!')
-    alert('Task Saved!')
-  })
-  //
-
+    //localStorage.setItem(`${hour}`, taskData);
+  //})
 });
